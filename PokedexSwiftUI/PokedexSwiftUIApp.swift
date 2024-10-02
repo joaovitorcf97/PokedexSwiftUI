30d52e86c3cb0e9b44376c7e1a23137078c9d2be
João Vitor <joaovitorcfdev@gmail.com>
2024-10-01 22:17:38 -0300

    Initial Commit

---
+++ PokedexSwiftUI/PokedexSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexSwiftUIApp.swift
+//  PokedexSwiftUI
+//
+//  Created by João Vitor Costa Ferreira on 01/10/24.
+//
+
+import SwiftUI
+
+@main
+struct PokedexSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PokedexSwiftUITests/PokedexSwiftUITests.swift
@@ -0,0 +1,16 @@
+//
+//  PokedexSwiftUITests.swift
+//  PokedexSwiftUITests
+//
+//  Created by João Vitor Costa Ferreira on 01/10/24.
+//
+
+import Testing
+
+struct PokedexSwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
